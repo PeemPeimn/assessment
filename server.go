894fc41400893e8cfd6bd2915903cbd2391827eb
Peemwaruch Intamool <intamoon.peem@gmail.com>
2022-12-24 00:49:59 +0700

    Add a middleware to authorize requests

--- server.go
+++ server.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/PeemPeimn/assessment/expenses"
 	"github.com/labstack/echo/v4"
 )
 
+// This function is a middleware function handling Token Authorization.
+// According to Postman's tests the validation value is "November 10, 2009".
+func authorizationHandler(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		auth := c.Request().Header.Get("Authorization")
+		if auth == "November 10, 2009" {
+			return next(c)
+		}
+		return c.JSON(http.StatusUnauthorized, "Invalid key value.")
+	}
+}
+
 func main() {
 	expenses.InitDB()
 
 	echoInstance := echo.New()
+
+	// Use the customized handler.
+	echoInstance.Use(authorizationHandler)
 
 	echoInstance.POST("/expenses", expenses.CreateExpensesHandler)
 

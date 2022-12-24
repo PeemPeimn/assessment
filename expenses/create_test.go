295198ff742c2ca5bb73abda16aab0baa230a486
Peemwaruch Intamool <intamoon.peem@gmail.com>
2022-12-24 21:42:54 +0700

    Add more unit test

--- expenses/create_test.go
+++ expenses/create_test.go
@@ -49,3 +49,37 @@ func TestCreateExpense(t *testing.T) {
 	assert.Equal(t, http.StatusCreated, rec.Code)
 	assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
 }
+
+func TestCreateExpenseFailCase(t *testing.T) {
+	// Arrange
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	newsMockRows := sqlmock.NewRows([]string{"id"}).AddRow(1)
+
+	mock.ExpectQuery("INSERT INTO expenses .*").WithArgs().WillReturnRows(newsMockRows)
+
+	e := echo.New()
+
+	mockJson := []byte(`{
+		"title": "smoothie",
+	  "amount": "79",
+	  "note": "abcd",
+	  "tags": ["food", "beverage"]
+		}`)
+
+	req := httptest.NewRequest(http.MethodPost, "/expenses", bytes.NewBuffer(mockJson))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	handler := Handler{DB: db}
+
+	// Act
+	handler.CreateExpense(c)
+
+	// Assert
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
+}

9521316d8136245d394bb23033add7dbed886a5a
Peemwaruch Intamool <intamoon.peem@gmail.com>
2022-12-24 17:14:40 +0700

    Add unit test for CreateExpenses

---
+++ expenses/create_test.go
@@ -0,0 +1,51 @@
+package expenses
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateExpenses(t *testing.T) {
+
+	// Arrange
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	newsMockRows := sqlmock.NewRows([]string{"id"}).AddRow(1)
+
+	mock.ExpectQuery("INSERT INTO expenses .*").WithArgs().WillReturnRows(newsMockRows)
+
+	e := echo.New()
+
+	mockJson := []byte(`{
+	  "title": "smoothie",
+	  "amount": 79,
+	  "note": "abcd",
+	  "tags": ["food", "beverage"]
+	}`)
+
+	req := httptest.NewRequest(http.MethodPost, "/expenses", bytes.NewBuffer(mockJson))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	handler := Handler{DB: db}
+
+	// Act
+	handler.CreateExpenses(c)
+
+	expected := `{"id":1,"title":"smoothie","amount":79,"note":"abcd","tags":["food","beverage"]}`
+
+	// Assert
+	assert.Equal(t, http.StatusCreated, rec.Code)
+	assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
+}

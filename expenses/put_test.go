e219c850bb5b475f54e68b811a4722df2523f6b9
Peemwaruch Intamool <intamoon.peem@gmail.com>
2022-12-27 11:31:53 +0700

    Add more unit test for PutExpense

--- expenses/put_test.go
+++ expenses/put_test.go
@@ -54,3 +54,42 @@ func TestPutExpense(t *testing.T) {
 	assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
 
 }
+
+func TestPutExpenseNotFound(t *testing.T) {
+	// Arrange
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	handler := Handler{DB: db}
+
+	newsMockRows := sqlmock.
+		NewRows([]string{"id", "title", "amount", "note", "tags"})
+
+	mock.ExpectPrepare("UPDATE expenses (.+) WHERE (.+) RETURNING (.+)").
+		ExpectQuery().WithArgs().WillReturnRows(newsMockRows)
+
+	mockJson := []byte(`{
+		"title": "smoothie",
+		"amount": 99,
+		"note": "unit_test",
+		"tags": ["put_test", "beverage"]
+		}`)
+
+	req := httptest.NewRequest(http.MethodPut, "/expenses/1", bytes.NewBuffer(mockJson))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	e := echo.New()
+	c := e.NewContext(req, rec)
+	c.SetParamNames("id")
+	c.SetParamValues("1")
+
+	// Act
+	handler.PutExpense(c)
+
+	// Assert
+	assert.Equal(t, http.StatusInternalServerError, rec.Code)
+
+}

72d5cba9fedfe9330e03149b47304b3612c571eb
Peemwaruch Intamool <intamoon.peem@gmail.com>
2022-12-27 11:37:20 +0700

    Implement PutExpense with Echo Instance in the main package

--- server.go
+++ server.go
@@ -38,6 +38,7 @@ func main() {
 
 	echoInstance.POST("/expenses", handler.CreateExpense)
 	echoInstance.GET("/expenses/:id", handler.GetExpenseByID)
+	echoInstance.PUT("/expenses/:id", handler.PutExpense)
 
 	// Start server
 	go func() {
